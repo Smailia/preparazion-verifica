909dc5ee3b4f2e2eaebe6f0d6fc7755a1d42f8b4
Smailia <smailia.omar@iisgalvanimi.edu.it>
2025-10-02 08:55:48 +0000

    preparazione

--- README.md
+++
@@ -1,1 +0,0 @@
-# preparazion-verifica
---
+++ angular-hello-world/src/app/first/first.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-app',
+  imports: [],
+  templateUrl: './first.html',
+  styleUrl: './first.css'
+})
+export class App {
+
+}
